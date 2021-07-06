516e931750ec29cd8d5807b13e7a1325744f1405
Nidrux <originalfroaz@gmail.com>
2021-07-06 16:50:52 +0200

    reworked captcha design
    Better font,  changed pattern to lay on top of the text

--- captcha.js
+++ captcha.js
@@ -9,36 +9,37 @@ let randomText = (length) => {
 }
 class Captcha {
     constructor(length) {
-        if(isNaN(length) || length < 5) {
+        if(isNaN(length) || !length) {
             var currentText = randomText(5);
+        } else if(length < 5 || length > 10){
+            console.log(`captcha: Given length '${length}' was too long. Defaulted to 10 (Min: 5 Max: 10)`);
+            var currentText = randomText(10);
         } else {
             var currentText = randomText(length);
         }
-        this.length = length
+        this.length = length;
         this.currentString = currentText;
     }
     async image(string) {
-        Canvas.registerFont('./assets/Good Brush.ttf', {family: 'Panic'});
+        Canvas.registerFont('./assets/captcha-font.ttf', {family: 'captcha'});
         const canvas = Canvas.createCanvas(500, 200);
         const ctx = canvas.getContext('2d');
         const patterns = [
-            'https://st4.depositphotos.com/5171687/31244/v/600/depositphotos_312440046-stock-video-seamless-young-animation-psychedelic-animal.jpg',
-            'https://cdn.dribbble.com/users/3185269/screenshots/10491989/trippy_art05_4x.png',
-            'https://st2.depositphotos.com/6703620/9627/i/950/depositphotos_96275896-stock-photo-black-and-white-marbling-background.jpg'
-        ]
+            'https://assets.mica.edu/files/resources/ultrawide-xsml-mica_pattern_organic.jpg',
+            'https://assets.mica.edu/files/resources/ultrawide-xsml-mica_pattern_linear_square-150px.jpg',
+            'https://thumbs.dreamstime.com/b/abstract-pattern-wavy-stripes-rippled-d-relief-black-white-lines-background-vector-twisted-curved-stripe-modern-trendy-103290482.jpg'
+        ];
+        ctx.font = '50px captcha';
+        let colors = ['#4e4ea3', '#22227d', '#0000cc', '#8e00fa'];
+        let minH = 50;
+        let maxH = canvas.height - minH;
+        ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+        ctx.fillText(string, canvas.height / 2,  Math.floor(Math.random() * maxH) +  minH);
+        ctx.globalAlpha = 0.3
         const pattern = await Canvas.loadImage(patterns[Math.floor(Math.random() * patterns.length)]);
         ctx.drawImage(pattern, 0, 0, canvas.width, canvas.height);
-        ctx.font = '50px Panic';
-        let splitStr = string.split('');
-        let colors = ['#4e4ea3', '#22227d', '#0000cc', '#8e00fa'];
-        for(i = 0; i < splitStr.length; i++) {
-            ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
-            let str = splitStr[i]
-            let padW = canvas.width / 4
-            let padH = canvas.height / 8
-            ctx.fillText(str, padW + i * 50,  50 + padH * Math.floor(Math.random() * string.length))
-        }
+        
         return canvas.toBuffer('image/png');
     }
 }
-module.exports.Captcha = Captcha
+module.exports.Captcha = Captcha;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Captcha }= require('./captcha');
 const { Client, MessageAttachment } = require('discord.js');
 const client = new Client();
 
-client.login('YOUR TOKEN HERE');
+client.login('');
 client.on('ready', () => {
     console.log(`${client.user.username} is ready`);
 })
@@ -10,9 +10,9 @@ client.on('message', async (message) => {
     if(message.author.bot) return;
     const { channel } = message;
 
-    const captcha = new Captcha();
+    const captcha = new Captcha(11);
     const captchaImage = new MessageAttachment(await captcha.image(captcha.currentString));
-
+    console.log(captcha.currentString);
     // DO SOMETHING WITH IMAGE
     channel.send(captchaImage);
 })
